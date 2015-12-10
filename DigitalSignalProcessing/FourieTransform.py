ee7ae5622a182dfc4bcbaa43f9e9bd3dde0330d7
R-Imai <s1426015eq@s.chibakoudai.jp>
2015-12-10 17:01:39 +0900

    add 3D plot and make plot command

--- DigitalSignalProcessing/FourieTransform.py
+++ DigitalSignalProcessing/FourieTransform.py
@@ -3,7 +3,7 @@
 #   Name:		FourieTransform.py
 #	Author:		R.Imai
 #	Created:	2015 / 11 / 27
-#	Last Date:	2015 / 12 / 08
+#	Last Date:	2015 / 12 / 10
 #	Note:
 #-------------------------------------------------------------------------------
 
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 import codecs
 from math import *
 import re
+from mpl_toolkits.mplot3d import Axes3D
+
 
 argv = sys.argv
 argNum = len(sys.argv)
@@ -36,9 +38,9 @@ def checkUsage():
     return check
 
 def usage():
-    print("\nコマンドライン引数: (入力ファイル名)(サンプリング間隔)(モード)(値1)(値2)(値3)")
+    print("\nコマンドライン引数: (入力ファイル名)(サンプリング間隔)(モード)(値1)(値2)(保存先ファイル名)(プロット内容)")
     print("\n\tモード:\t出力:\tp: その場にプロット")
-    print("\t　　  \t　　 \ts: 保存(コマンドラインの最後に保存したいファイル名を拡張子なしで指定)")
+    print("\t　　  \t　　 \ts: 保存((保存先ファイル名)に保存したいファイル名を拡張子なしで指定)")
     print("\t　　  \t範囲:\t-a: すべての区間\t")
     print("\t　　  \t　　 \t-n: (値1)にNの値を指定。最初からN間隔でフーリエ変換")
     print("\t　　  \t　　 \t-s: (値1)に開始点を指定。そこから最後まで。")
@@ -49,8 +51,18 @@ def usage():
     print("\t　　  \t　　　:\t-bk: ブラックマン窓")
     print("\t　　  \t　　　:\t-ga: ガウス窓")
     print("\t　　  \t　　　:\t-bar: バートレット窓")
-    print("\tモードは(出力)(範囲)(窓関数)で記述\n\tex)p-a-hm: すべての区間にハミング窓をかけその場に表示")
+    print("\tモードは(出力)(範囲)(窓関数)で記述\n\tex)p-a-hm: すべての区間にハミング窓をかけその場に表示\n")
+    print("\tプロット内容:\t出力内容を指定する。2Dなら最大4つまででそれぞれの間は-でつなぐ(記述がない場合はデフォルト値(o-w-p-f)になる)")
+    print("\t　　  \t　　　:\to: オリジナルの信号")
+    print("\t　　  \t　　　:\tr: フーリエ実数部")
+    print("\t　　  \t　　　:\ti: フーリエ虚数部")
+    print("\t　　  \t　　　:\tw: 窓関数")
+    print("\t　　  \t　　　:\tf: フーリエ変換結果")
+    print("\t　　  \t　　　:\tp: 窓関数をかけた後の信号")
+    print("\t　　  \t　　　:\t3D: 3Dプロット(3Dの場合は一つだけ)")
+
     exit()
+
 
 def importData():
     print("importing...")
@@ -106,26 +118,76 @@ def makePalam(y):
         N = len(y)
         if mode[0] == "s":
             exFileName = argv[4]
+            try:
+                palamList = argv[5]
+            except IndexError:
+                palamList = "o-w-p-f"
+        elif mode[0] == "p":
+            try:
+                palamList = argv[4]
+            except IndexError:
+                palamList = "o-w-p-f"
+
     elif mode[1] == "n":
         start = "n"
         N = int(argv[4])
         if mode[0] == "s":
             exFileName = argv[5]
+            try:
+                palamList = argv[6]
+            except IndexError:
+                palamList = "o-w-p-f"
+        elif mode[0] == "p":
+            try:
+                palamList = argv[5]
+            except IndexError:
+                palamList = "o-w-p-f"
+
     elif mode[1] == "s":
         start = int(argv[4])
         N = len(y) - int(argv[4])
         if mode[0] == "s":
             exFileName = argv[5]
+            try:
+                palamList = argv[6]
+            except IndexError:
+                palamList = "o-w-p-f"
+        elif mode[0] == "p":
+            try:
+                palamList = argv[5]
+            except IndexError:
+                palamList = "o-w-p-f"
+
     elif mode[1] == "f":
         start = 0
         N = int(argv[4])
         if mode[0] == "s":
             exFileName = argv[5]
+            try:
+                palamList = argv[6]
+            except IndexError:
+                palamList = "o-w-p-f"
+        elif mode[0] == "p":
+            try:
+                palamList = argv[5]
+            except IndexError:
+                palamList = "o-w-p-f"
+
     elif mode[1] == "p":
         start = int(argv[4])
         N = int(argv[5]) - int(argv[4])
         if mode[0] == "s":
             exFileName = argv[6]
+            try:
+                palamList = argv[7]
+            except IndexError:
+                palamList = "o-w-p-f"
+        elif mode[0] == "p":
+            try:
+                palamList = argv[6]
+            except IndexError:
+                palamList = "o-w-p-f"
+
     else :
         usage()
 
@@ -134,7 +196,26 @@ def makePalam(y):
     else:
         win = mkWindow("rect",N)
 
-    return start,N,win
+    return start,N,win,palamList
+
+def getPlotPalam(palamList):
+    trans = {"o":0,"w":1,"r":2,"i":3,"f":4,"p":5}
+    if argv[argNum - 1]:
+        arg = re.split(r"[-]",palamList)
+    if arg[0] == "3D" or arg[0] == "3d":
+        palam = "3d"
+    else:
+        palam = []
+        try:
+            for i in range(4):
+                if i < len(arg):
+                    palam.append(trans[arg[i]])
+        except :
+            print("プロット内容の指定がおかしいです。")
+            exit()
+    return palam
+
+
 
 
 def broach(y, start, n):
@@ -153,44 +234,76 @@ def FFT(y,dt):
     return sig,freq
 
 
-def plot(base,coe1,coe2,coe3,Hz,start,end,n):
-    print("ploting...")
+
+
+def detailPlot(palam, start, end, leng, Hz):
+    if palam == 0:
+        plt.axvline(start, color = palette[12],label = "start")
+        plt.axvline(end, color = palette[11],label = "end")
+        plt.xlim([0,leng-1])
+        plt.xlabel(u"number of data")
+        plt.ylabel(u"power")
+        plt.title("origin signal")
+    elif palam == 1:
+        plt.xlim([0,leng-1])
+        plt.xlabel(u"number of data")
+        plt.ylabel(u"power")
+        plt.title("window function")
+    elif palam == 2:
+        plt.xticks(np.linspace(0,(leng//16)*16,17),np.linspace(0,(leng//16)*16*Hz/(end-start),17).astype(np.int))
+        plt.xlim([0,leng-1])
+        plt.xlabel(u"frequency [kHz]")
+        plt.ylabel(u"power")
+        plt.title("real part")
+    elif palam == 3:
+        plt.xticks(np.linspace(0,(leng//16)*16,17),np.linspace(0,(leng//16)*16*Hz/(end-start),17).astype(np.int))
+        plt.xlim([0,leng-1])
+        plt.xlabel(u"frequency [kHz]")
+        plt.ylabel(u"power")
+        plt.title("imaginary part")
+    elif palam == 4:
+        plt.xticks(np.linspace(0,(leng//16)*16,17),np.linspace(0,(leng//16)*16*Hz/(end-start),17).astype(np.int))
+        plt.xlim([0,int((leng-1)/2)])
+        plt.xlabel(u"frequency [kHz]")
+        plt.ylabel(u"power")
+        plt.title("fourie transform")
+    elif palam == 5:
+        plt.xlim([0,leng-1])
+        plt.xlabel(u"number of data")
+        plt.ylabel(u"power")
+        plt.title("processing signal")
+
+
+def plot2D(plotPalam, data,Hz,start,end,n):
+    print("ploting2D...")
     plt.figure(figsize=(16, 9))
+    winDict = [0,110,210,220,220]
 
-    plt.subplot(221)
-    plt.plot(base,color = palette[0])
-    plt.axvline(start, color = palette[12],label = "start")
-    plt.axvline(end, color = palette[11],label = "end")
-    plt.xlim([0,len(base)-1])
-    plt.xlabel(u"number of data")
-    plt.ylabel(u"power")
-    plt.title("origin signal")
+    for i in range(len(plotPalam)):
+        plt.subplot(winDict[len(plotPalam)] + i + 1)
+        plt.plot(data[plotPalam[i]] ,color = palette[i])
+        detailPlot(plotPalam[i],start,end,len(data[plotPalam[i]]),Hz)
 
-
-    plt.subplot(222)
-    plt.plot(coe1,color = palette[1])
-    plt.xlim([0,len(coe1)-1])
-    plt.xlabel(u"number of data")
-    plt.ylabel(u"power")
-    plt.title("window function")
+    if output == "p":
+        plt.show()
+        print("\tsucess!")
+        plt.clf
+    elif output == "s":
+        plt.savefig(exFileName + str(n) + ".png")
+        print("\tsave " + exFileName + str(n) + ".png")
+        plt.close()
 
 
-    plt.subplot(223)
-    plt.plot(coe2,color = palette[2])
-    plt.xlim([0,len(coe2)-1])
-    plt.xlabel(u"number of data")
-    plt.ylabel(u"power")
-    plt.title("processing signal")
+def plot3D(re,im,N,Hz,n):
+    print("ploting3D...")
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot3D(np.linspace(1, N, N), re, im)
+    plt.xticks(np.linspace(0,(N//16)*16,17),np.linspace(0,(N/16)*16*Hz/N,17).astype(np.int))
+    ax.set_xlabel("frequency [kHz]")
+    ax.set_ylabel("real parts power")
+    ax.set_zlabel("imaginary parts power")
 
-
-    plt.subplot(224)
-    plt.plot(coe3,color = palette[3])
-    #plt.yscale("log")
-    plt.xticks(np.linspace(0,(len(coe3)//16)*16,17),np.linspace(0,(len(coe3)//16)*16*Hz/(end-start),17).astype(np.int))
-    plt.xlim([0,int((len(coe2)-1)/2)])
-    plt.xlabel(u"frequency [kHz]")
-    plt.ylabel(u"power")
-    plt.title("fourie transform")
     if output == "p":
         plt.show()
         print("\tsucess!")
@@ -206,7 +319,7 @@ if __name__ == '__main__':
     if checkUsage():
         usage()
     y = importData()
-    start,N,win = makePalam(y)
+    start,N,win,palamList = makePalam(y)
     dt = int(argv[2])
     if start == "n":
         for i in range((len(y)//N)*2):
@@ -214,11 +327,19 @@ if __name__ == '__main__':
             yCut = broach(y, start, N)
             winY = yCut * win
             sig,freq = FFT(yCut,dt)
-            #plot(y,np.real(sig),np.imag(sig),np.abs(sig),dt,start,start + N,i)
-            plot(y,win,winY,np.abs(sig),dt,start,start + N, i)
+            plotPalam = getPlotPalam(palamList)
+            sendData = [y, win, np.real(sig), np.imag(sig), np.abs(sig), winY]
+            if plotPalam == "3d":
+                plot3D(np.real(sig),np.imag(sig),N,dt,i)
+            else:
+                plot2D(plotPalam, sendData,dt,start,start + N, i)
     else :
         yCut = broach(y, start, N)
         winY = yCut * win
         sig,freq = FFT(yCut,dt)
-        #plot(y,np.real(sig),np.imag(sig),np.abs(sig),dt,start,start + N,"")
-        plot(y,win,winY,np.abs(sig),dt,start,start + N,"")
+        plotPalam = getPlotPalam(palamList)
+        sendData = [y, win, np.real(sig), np.imag(sig), np.abs(sig), winY]
+        if plotPalam == "3d":
+            plot3D(np.real(sig),np.imag(sig),N,dt,"")
+        else:
+            plot2D(plotPalam, sendData,dt,start,start + N,"")
